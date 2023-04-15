351750b80fff2793f18dac3175f49bc4a03f37a9
RenanJ01 <jonatasrenan02@gmail.com>
2023-04-15 16:09:35 -0300

    up

--- php/Access/cadastrar.php
+++ php/Access/cadastrar.php
@@ -1,5 +1,6 @@
 <?php
-    include './Assets/conection.php';
+    require_once("..\Assets\conection.php");
+    $conc = new PDO("mysql: host=localhost:8080; db=elgames", "clientes", "usuariostigresa1001");
 
 ?>
 
@@ -23,22 +24,22 @@
             <label for="fname">Nome:</label><br>
             <input type="text" id="fname" name="fname" required><br><br>
     
-            <label for="fuser">Nome:</label><br>
+            <label for="fuser">Username:</label><br>
             <input type="text" id="fuser" name="fuser" required><br><br>
-
-            <label for="fgenero">Genero:</label><br>
-            <input type="radio" id="fgenero" name="fgenero" checked="true" value="Masculino" required><label for="mas">Masculino</label><br>
-            <input type="radio" id="fgenero" name="fgenero" value="Feminino"><label for="fem">Feminino</label><br>
-            <input type="radio" id="fgenero" name="fgenero" value="Outro"><label for="out">Outro</label><br><br>
 
             <label for="fpass">Senha:</label><br>
             <input type="password" id="fpass" name="fpass" required><br><br>
 
-            <label for="fidade">Idade:</label><br>
-            <input type="number" id="fidade" name="fidade" value="1" min="1" max="200" required><br><br>
+            <label for="fgenero">Genero:</label><br>
+            <input type="radio" id="fgenero" name="fgenero" checked="true" value="M"><label for="mas">Masculino</label><br>
+            <input type="radio" id="fgenero" name="fgenero" value="F"><label for="fem">Feminino</label><br>
+            <input type="radio" id="fgenero" name="fgenero" value="O"><label for="out">Outro</label><br><br>
 
-            <input class="btn_action" type="submit" value="Cadastrar"> 
-            <input class="btn_action" type="reset" value="Redefinir"><br>
+            <label for="fidade">Idade:</label><br>
+            <input type="number" id="fidade" name="fidade" value="1" min="1" max="127"/><br><br>
+
+            <input class="btn_action" type="submit" value="Cadastrar"/> 
+            <input class="btn_action" type="reset" value="Redefinir"/><br>
         </fieldset>
     </form>
     </main>
--- php/Assets/conection.php
+++ php/Assets/conection.php
@@ -2,53 +2,83 @@
     class Conexao{
         private $conection;
 
-        private function AbrirConection(){
+        public function Conc_AbrirConection(){
             try {
-                $this->conection = new PDO("mysql: host=localhost:8080; db=usuarios", "clientes", "usuariostigresa1001");
+                $this->conection = new PDO("mysql: host=localhost:8080; db=elgames", "clientes", "usuariostigresa1001");
                 $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //return $this->conection;
                 echo "Sucesso na conexão!";
             } catch (PDOException $e) {
                 echo "Falha na conexão:".$e->getMessage();
+                //return false;
             }            
         }
 
-        public function Insert($name, $username, $senha){
+        public function Conc_Select($query){
             try {
-                $this->AbrirConection();
+                $con = $this->Conc_AbrirConection();
+                /*$res = $con->query($query);
+                if($res){
+                    $res->setFetchMode(PDO::FETCH_COLUMN, 0);
+                    $tab = $res->fetchAll();
+                    return $tab;
+                }else{
+                    return false;
+                }*/
+            } catch (Exception $e) {
+                echo "Ocorreu o erro:".$e->getMessage();
+            }            
+        }
+
+        public function Conc_Insert($name, $username, $senha){
+            try {
+                $con = $this->Conc_AbrirConection();
+                /*$res = $con->query("");
+                if($res){
+                    return true;
+                }else{
+                    return false;
+                }*/
+            } catch (Exception $e) {
+                echo "Ocorreu o erro:".$e->getMessage();
+            }            
+        }
+
+        public function Conc_Update($name, $username, $senha){
+            try {
+                $con = $this->Conc_AbrirConection();
                 echo "A ação foi concluida com exito.";
             } catch (Exception $e) {
                 echo "Ocorreu o erro:".$e->getMessage();
             }            
         }
 
-        public function Insert_2($name, $username, $senha, $genero){
+        public function Conc_Insert_2($name, $username, $senha, $genero){
             try {
-                $this->AbrirConection();
+                $con = $this->Conc_AbrirConection();
                 echo "A ação foi concluida com exito.";
             } catch (Exception $e) {
                 echo "Ocorreu o erro:".$e->getMessage();
             }            
         }
 
-        public function Insert_3($name, $username, $senha, $idade){
+        public function Conc_Insert_3($name, $username, $senha, $idade){
             try {
-                $this->AbrirConection();
+                $con = $this->Conc_AbrirConection();
                 echo "A ação foi concluida com exito.";
             } catch (Exception $e) {
                 echo "Ocorreu o erro:".$e->getMessage();
             }            
         }
 
-        public function Insert_total($name, $username, $senha, $genero, $idade){
+        public function Conc_Insert_total($name, $username, $senha, $genero, $idade){
             try {
-                $this->AbrirConection();
+                $con = $this->Conc_AbrirConection();
                 echo "A ação foi concluida com exito.";
             } catch (Exception $e) {
                 echo "Ocorreu o erro:".$e->getMessage();
             }            
         }
-
-        
 
     }
 ?>
